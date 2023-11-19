986e13eb73a639c667b4442305e7b009d654b3f5
dimariga <145824625+dimariga@users.noreply.github.com>
2023-11-19 19:52:57 +0300

    Update train.py

--- train.py
+++ train.py
@@ -6,10 +6,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import make_column_transformer
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestRegressor
-import json
-import joblib
-
-PATH_UNIQUE_VALUES = "unique_values.json"
+import streamlit as st
 
 df = pd.read_csv("flats.csv")
 categorical_features = ["city", "house_wall_type", "renovation"]
@@ -34,12 +31,76 @@ X = df.drop(columns="price")
 y = df["price"]
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=10)
 preprocessor = make_column_transformer((StandardScaler(), numeric_feautures), (OneHotEncoder(handle_unknown="ignore", drop="first"), categorical_features))
-clf = make_pipeline(preprocessor, RandomForestRegressor(max_depth=15, random_state=10))
-clf.fit(X_train, y_train)
-df.to_csv("flats2.csv", index=False)
+model = make_pipeline(preprocessor, RandomForestRegressor(max_depth=15, random_state=10))
+model.fit(X_train, y_train)
 
-PATH_MODEL = "lr_pipeline.sav"
-joblib.dump(clf, PATH_MODEL)
 dict_unique = {key: X[key].unique().tolist() for key in X.columns}
-with open(PATH_UNIQUE_VALUES, "w") as file:
-    json.dump(dict_unique, file)
+
+st.header('Цена квартиры')
+
+st.write(df[:4])
+
+
+city = st.sidebar.selectbox("Город", (dict_unique["city"]))
+house_wall_type = st.sidebar.selectbox("Тип стен", (dict_unique["house_wall_type"]))
+renovation = st.sidebar.selectbox("Ремонт", (dict_unique["renovation"]))
+
+
+area = st.sidebar.slider(
+    "Площадь",
+    min_value=min(dict_unique["area"]),
+    max_value=max(dict_unique["area"])
+)
+
+rooms = st.sidebar.slider(
+    "Количество комнат",
+    min_value=min(dict_unique["rooms"]),
+    max_value=max(dict_unique["rooms"])
+)
+
+floor = st.sidebar.slider(
+    "Этаж",
+    min_value=min(dict_unique["floor"]),
+    max_value=max(dict_unique["floor"])
+)
+
+house_floors = st.sidebar.slider(
+    "Этажность дома",
+    min_value=min(dict_unique["house_floors"]),
+    max_value=max(dict_unique["house_floors"])
+)
+kitchen_area = st.sidebar.slider(
+    "Площадь кухни",
+    min_value=min(dict_unique["kitchen_area"]),
+    max_value=max(dict_unique["kitchen_area"])
+)
+
+balconies = st.sidebar.slider(
+    "Количество балконов",
+    min_value=min(dict_unique["balconies"]),
+    max_value=max(dict_unique["balconies"])
+)
+
+lifts = st.sidebar.slider(
+    "Количество лифтов",
+    min_value=min(dict_unique["lifts"]),
+    max_value=max(dict_unique["lifts"])
+)
+
+dict_data = {
+    "city": city,
+    "house_wall_type": house_wall_type,
+    "renovation": renovation,
+    "area": area,
+    "rooms": rooms,
+    "floor": floor,
+    "house_floors": house_floors,
+    "kitchen_area": kitchen_area,
+    "balconies": balconies,
+    "lifts": lifts,
+}
+data_predict = pd.DataFrame([dict_data])
+button = st.button("Предварительная цена")
+if button:
+    result = model.predict(data_predict)[0]
+    st.write(result)
